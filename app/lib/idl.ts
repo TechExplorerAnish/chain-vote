c15954470e788e9529abad6325c4bcfdbadf7c14
TechExplorerAnish <anishghimire178@gmail.com>
2026-03-01 16:01:43 +0545

    style: format code for consistency in idl.ts and vercel.json

--- app/lib/idl.ts
+++ app/lib/idl.ts
@@ -6,12 +6,12 @@
 let rawIdl: any;
 
 try {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  rawIdl = require("../../target/idl/chain_vote.json");
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    rawIdl = require("../../target/idl/chain_vote.json");
 } catch {
-  // If IDL file is not available (e.g., in production build), use an empty object
-  // The program will need to fetch the IDL from the blockchain at runtime
-  rawIdl = {};
+    // If IDL file is not available (e.g., in production build), use an empty object
+    // The program will need to fetch the IDL from the blockchain at runtime
+    rawIdl = {};
 }
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
